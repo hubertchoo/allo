6d51901506ab8d39f4491a9159b4a65eff1ac472
Xinyu Yang <xy494@zhang-21.ece.cornell.edu>
2025-02-03 17:17:11 -0400

    test support for int and float

--- tutorials/xsim_test/tutorial_vhls_xsim_int.py
+++ tutorials/xsim_test/tutorial_vhls_xsim_int.py
@@ -17,7 +17,7 @@ First, we import the necessary packages.
 """
 
 import allo
-from allo.ir.types import float32
+from allo.ir.types import int32
 import numpy as np
 
 ##############################################################################
@@ -48,8 +48,8 @@ M, N, K = 32, 32, 32
 # this information to generate correct intermediate buffers.
 
 
-def gemm(A: float32[M, K], B: float32[K, N]) -> float32[M, N]:
-    C: float32[M, N] = 0.0
+def gemm(A: int32[M, K], B: int32[K, N]) -> int32[M, N]:
+    C: int32[M, N] = 0
     for i, j in allo.grid(M, N):
         for k in allo.reduction(K):
             C[i, j] += A[i, k] * B[k, j]
@@ -141,7 +141,21 @@ print(code)
 # the HLS project and invoke the compiler to generate the RTL design.
 
 mod = s.build(target="vitis_hls", mode="csyn_xsim", project="gemm_xsim.prj")
-mod()
+# mod()
+A = np.random.randint(0, 10, size=(M, K), dtype=np.int32)
+B = np.random.randint(0, 10, size=(K, N), dtype=np.int32)
+output = np.zeros((M, N)).astype(np.int32)
+mod(A, B, output)
+print(output)
+
+expected = np.dot(A, B)
+abs_diff = np.abs(output - expected)
+rel_diff = abs_diff / (np.abs(expected) + 1e-10)
+max_abs_diff = np.max(abs_diff)
+max_rel_diff = np.max(rel_diff)
+print(f"Max Absolute Difference: {max_abs_diff}")
+print(f"Max Relative Difference: {max_rel_diff}")
+np.testing.assert_allclose(output, expected)
 
 # %%
 # You will see a ``gemm.prj`` folder is generated in the current directory:
